e467d03125a7c00aec69b1173948f60f8fc01d07
siddhartha <sid@hotelly.com.au>
2024-05-21 22:52:58 +0530

    Initial Commit

---
+++ BackupCommissioning/BackupCommissioningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackupCommissioningApp.swift
+//  BackupCommissioning
+//
+//  Created by sid on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BackupCommissioningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackupCommissioning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BackupCommissioning
+//
+//  Created by sid on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
